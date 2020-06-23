1baffb084876e02a3a6c29a5b738a5a494679b8f
msalvati <m.salvati@arpalombardia.it>
2020-06-23 14:05:29 +0000

    corretto ciclo per calcolo min med max

--- scriptR/prova_min_med_max.R
+++ scriptR/prova_min_med_max.R
@@ -18,8 +18,8 @@ library(rgdal)
 
 #mese<-"06" 
 #anno<-"2015"
-for (anno in seq(2002,2019) {
-for (mese in c("01","02")) { 
+for (anno in seq(2002,2019)) {
+for (mese in c("01","02","03","04","05","06","07","08","09","10","11","12")) { 
 #trovo la directory giusta
 cartella<-paste("../T2m/dati_orari/",anno,mese,sep="")
 print(cartella)
@@ -28,9 +28,9 @@ nomefile<-list.files(path=cartella)
 numero_file=length(nomefile)
 numero_giorni=numero_file/24
 print(numero_giorni)
-if (numero_giorni < 28 or numero_giorni > 31 ) { 
+if (numero_giorni < 28 ||  numero_giorni > 31 ) { 
   print(paste("Mese: ",mese," e anno: ",anno," con dati incompleti (trovo ",numero_giorni*24," files.",sep=""))
-  quit()
+  #quit()
 } 
 datainizio<-strptime(paste(anno,mese,"01","01",sep=""),"%Y%m%d%H")
 print(datainizio)
@@ -63,3 +63,4 @@ print(datainizio)
 } # fine ciclo giorni
 } # fine ciclo mesi
 }
+q()
