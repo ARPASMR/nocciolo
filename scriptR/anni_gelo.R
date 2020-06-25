7dd07077799475bac55d293bd80afd9d3d6fe456
msalvati <m.salvati@arpalombardia.it>
2020-06-25 16:39:21 +0000

    calcolo giorni gelo && aggiunto raster orografia

---
+++ scriptR/anni_gelo.R
@@ -0,0 +1,44 @@
+#---------------------------------------
+#
+# calcolo giorni con Tmin < -10° C
+#
+#---------------------------------------
+
+#librerie
+#-------------------------
+library(raster)
+library(sp)
+library(rgdal)
+
+# apro il primo raster e lo etto a NA per usare geometria originale
+cartella <- "../T2m/min_giorno/"
+
+#qui mettere il ciclo
+for (anno in seq(2002,2019)) {
+
+ggelo <-raster(paste(cartella,"Tmin20020101.asc",sep=""))
+values(ggelo)<-0
+names(ggelo)<-"ggelo"
+
+somma <-raster(paste(cartella,"Tmin20020101.asc",sep=""))
+values(somma)<-0
+names(somma)<-"somma"
+
+#elenco file da processare
+
+comando <- paste("ls -1 ",cartella,"Tmin",anno,"*.asc",sep="")
+nomefile <- readLines(pipe(comando))
+i<-1
+while ( i <= length(nomefile) ) {
+  print(nomefile[i]) # per debug
+  temp<-raster(nomefile[i])
+  values(somma)[which(values(temp) < -10)] <- 1
+  print(max(somma))
+  ggelo<-ggelo+somma
+  values(somma)<-0
+  i<-i+1
+} #fine ciclo file dell'anno
+
+writeRaster(ggelo, file=paste("../indici/ggelo_",anno,".txt",sep=""), format="ascii", overwrite=T)
+
+} #fine ciclo anni
