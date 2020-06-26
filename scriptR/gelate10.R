547d457b018b56547c6a10f636c6980643056050
msalvati <m.salvati@arpalombardia.it>
2020-06-26 11:06:17 +0000

    calcolo indice gelate invernali

---
+++ scriptR/gelate10.R
@@ -0,0 +1,33 @@
+#---------------------------------------
+#
+# calcolo giorni con Tmin < -10° C
+#
+#---------------------------------------
+
+#librerie
+#-------------------------
+library(raster)
+library(sp)
+library(rgdal)
+
+# apro il primo raster e lo etto a NA per usare geometria originale
+cartella <- "../indici/"
+
+gelate <-raster(paste(cartella,"ggelo_2002.asc",sep=""))
+values(gelate)<-0
+names(gelate)<-"gelate-10"
+
+#qui mettere il ciclo
+numero_anni <- 0
+for (anno in seq(2002,2019)) {
+numero_anni<-numero_anni+1
+temporaneo <-raster(paste(cartella,"ggelo_",anno,".asc",sep=""))
+values(temporaneo)[which(values(temporaneo)>0)] <- 1
+values(gelate) <- values(gelate)+values(temporaneo)
+}
+
+values(gelate)<-values(gelate)/numero_anni*100
+
+writeRaster(gelate, file="../gelate10.asc", format="ascii", overwrite=T)
+
+q()
