405a968ac95531ee257c2880df94323573371be0
msalvati <m.salvati@arpalombardia.it>
2020-08-21 13:48:18 +0000

    calcolo solo su  mesi invernali preservando NA

--- scriptR/anni_gelo.R
+++ scriptR/anni_gelo.R
@@ -17,16 +17,18 @@ cartella <- "../T2m/min_giorno/"
 for (anno in seq(2002,2019)) {
 
 ggelo <-raster(paste(cartella,"Tmin20020101.asc",sep=""))
-values(ggelo)<-0
+values(ggelo)[which(!is.na(values(ggelo)))] <- 0
 names(ggelo)<-"ggelo"
 
 somma <-raster(paste(cartella,"Tmin20020101.asc",sep=""))
-values(somma)<-0
+values(somma)<-NA
+values(somma)[which(!is.na(values(ggelo)))] <- 0
 names(somma)<-"somma"
 
 #elenco file da processare
 
-comando <- paste("ls -1 ",cartella,"Tmin",anno,"*.asc",sep="")
+comando <- paste("ls -1 ",cartella,'Tmin',anno,'*.asc | grep "\\(01\\|02\\|12\\)..\\.asc"  -',sep="")
+print(comando)
 nomefile <- readLines(pipe(comando))
 i<-1
 while ( i <= length(nomefile) ) {
